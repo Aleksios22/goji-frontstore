f18ad8225adab8547acef4feb8f5f3b03228ef7e
unknown <alexisrochon98@outlook.com>
2022-04-06 01:02:23 -0400

    base

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Goji store front-end
 
 https://www.alpinetoolbox.com/
+
+https://www.hyperui.dev/
+
+https://www.alptail.com/all/
+
+https://github.com/saadeghi/theme-change
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,30 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        gojiTheme: {
+          "primary": "#570DF8",
+          "secondary": "#F000B8",
+          "accent": "#37CDBE",
+          "neutral": "#3D4451",
+          "base-100": "#FFFFFF",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
 }
